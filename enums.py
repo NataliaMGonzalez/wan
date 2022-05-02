761abfa9bb30adb0a5602b7ba8114387db4c9f69
Natalia MGH <nataliagzzh@gmail.com>
2022-05-01 23:30:54 -0500

    Add Operators to enums

--- enums.py
+++ enums.py
@@ -6,3 +6,16 @@ class DataTypes(Enum):
     INT = "int"
     FLOAT = "float"
     CHAR = "char"
+
+
+class Operators(Enum):
+    ADD = "+"
+    MINUS = "-"
+    MULTIPLY = "*"
+    DIVIDE = "/"
+    REL_OP_GT = ">"
+    REL_OP_LT = "<"
+    REL_OP_NE = "<>"
+    REL_OP_EQ = "=="
+    LOG_OP_AND = "&&"
+    LOG_OP_OR = "||"

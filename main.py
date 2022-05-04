49a6498b8dd9344318b0c131c02a8266ee780f78
Diego Estrada Talamantes <devdiegoest@gmail.com>
2022-05-04 11:54:03 -0500

    Created examples folder and changes the one used in main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from lark_parser import parseTree
 
 grammar = open("grammar.lark", 'r').read()
-code = open("example.wan", 'r').read()
+code = open("examples/only-expressions.wan", 'r').read()
 
 tree = parseTree(grammar, code)
 print(tree.pretty())

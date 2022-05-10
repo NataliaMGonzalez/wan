d1fe7db7a1a6b63b7118deb84d00f6697b1764c5
Diego Estrada Talamantes <devdiegoest@gmail.com>
2022-05-10 17:48:46 -0500

    Change quadruples to visit recursive

--- quadruples_linear_statements.py
+++ quadruples_linear_statements.py
@@ -1,61 +1,42 @@
 from enums import Operators
-from lark import Visitor
+from lark.visitors import Visitor_Recursive
 
 
 def generate_quadruples(tree):
-    Quadruples().visit_topdown(tree)
+    Quadruples().visit(tree)
     return Quadruples.quadruples
 
 
-class Quadruples(Visitor):
+class Quadruples(Visitor_Recursive):
     quadruples = []
     stackAddresses = []
-    stackOperators = []
     temp_count = 1
 
-    def expression(self, tree):
-        print(tree.pretty())
-
-    def np_add_quadruple(self, _tree):
+    def add_quadruple(self, tree):
         right_operand = self.stackAddresses.pop()
         left_operand = self.stackAddresses.pop()
-        operator = self.stackOperators.pop()
+        operator = Operators(tree.children[1].value)
         temp_name = "t{}".format(self.temp_count)
         quad = [operator, left_operand, right_operand, temp_name]
         self.quadruples.append(quad)
         self.stackAddresses.append(temp_name)
         self.temp_count += 1
 
-    def np_add_assignment_quadruple(self, _tree):
-        right_operand = self.stackAddresses.pop()
-        left_operand = self.stackAddresses.pop()
-        operator = self.stackOperators.pop()
-        quad = [operator, left_operand, right_operand]
-        self.quadruples.append(quad)
+    def or_expression(self, tree):
+        self.add_quadruple(tree)
 
-    def assignment_operator(self, tree):
-        self.stackOperators.append(tree.children[0].value)
+    def and_expression(self, tree):
+        self.add_quadruple(tree)
 
-    def or_expression_operator(self, tree):
-        self.stackOperators.append(tree.children[0].value)
+    def comp_expression(self, tree):
+        self.add_quadruple(tree)
 
-    def and_expression_operator(self, tree):
-        self.stackOperators.append(tree.children[0].value)
+    def sum_expression(self, tree):
+        self.add_quadruple(tree)
 
-    def relop(self, tree):
-        self.stackOperators.append(tree.children[0].value)
-
-    def sum_expression_operator(self, tree):
-        self.stackOperators.append(tree.children[0].value)
-
-    def term_operator(self, tree):
-        self.stackOperators.append(tree.children[0].value)
-
-    def assignment_var(self, tree):
-        # Search for memory address of var and append the address
-        self.stackAddresses.append(tree.children[0].value)
+    def term(self, tree):
+        self.add_quadruple(tree)
 
     def numerical_constant(self, tree):
-        # Add numerical constant to memory and append the address
         num_const = tree.children[1].value
         self.stackAddresses.append(num_const)

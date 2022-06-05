c082682ed65ba313d52aa07a52a808671b7193b7
Diego Estrada Talamantes <devdiegoest@gmail.com>
2022-06-05 11:30:31 -0500

    Allow assigning classes as parameters

--- raava/assignments.py
+++ raava/assignments.py
@@ -12,4 +12,13 @@ def execute_assignment(quadruple: Tuple[Operators, int, int, int]):
     _, new_address, original_address = quadruple
     new_address = get_address(new_address)
     original_address = get_address(original_address)
+    if is_class(new_address) and is_class(original_address):
+        memory[new_address] = (original_address,)
+        return
     memory[new_address] = memory[original_address]
+
+
+def is_class(address):
+    if address >= 5000 and address < 6000:
+        return True
+    return False

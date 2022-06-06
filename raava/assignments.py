d0e168639819a737d966122fef33b30eece7f2a0
Diego Estrada Talamantes <devdiegoest@gmail.com>
2022-06-06 18:12:18 -0500

    Small correction in VM assignments module

--- raava/assignments.py
+++ raava/assignments.py
@@ -1,6 +1,6 @@
 from typing import Tuple
-from addresses_manager import CLASS_START_POSITION, DS_RESERVED_MEMORY, get_segment_size
-from enums import DataTypes, Operators
+from addresses_manager import is_class
+from enums import Operators
 import globals
 from raava.memory import get_address
 
@@ -17,11 +17,3 @@ def execute_assignment(quadruple: Tuple[Operators, int, int, int]):
         memory[new_address] = (original_address,)
         return
     memory[new_address] = memory[original_address]
-
-
-def is_class(address):
-    CLASS_END_POSITION = CLASS_START_POSITION + \
-        get_segment_size(DS_RESERVED_MEMORY[DataTypes.CHAR])
-    if address >= CLASS_START_POSITION and address < CLASS_END_POSITION:
-        return True
-    return False

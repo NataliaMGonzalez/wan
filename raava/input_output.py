c8edda8738b65c48d05b3292ea43556f4616bbc8
Diego Estrada Talamantes <devdiegoest@gmail.com>
2022-06-06 21:00:37 -0500

    Parse data in the input_output

--- raava/input_output.py
+++ raava/input_output.py
@@ -1,6 +1,8 @@
-from typing import Tuple
+import re
+from typing import Tuple, Union
 from enums import InputOutputInstructions
 import globals
+from quadruples.constants import str_to_bool
 from raava.memory import get_address
 
 
@@ -27,4 +29,21 @@ def execute_read(quadruple: Tuple[InputOutputInstructions, int]):
     """Executes the inputting of the given address."""
     _, address = quadruple
     address = get_address(address)
-    memory[address] = input(">> ")
+    value: str = input(">> ")
+    return_value = adapt_to_type(value)
+    memory[address] = return_value
+
+
+def adapt_to_type(value: str) -> Union[str, int, float, bool]:
+    """Based on the string, parse to the appropriate type."""
+    return_value = value
+    is_int = re.search(r"^[0-9]+$", value)
+    if is_int:
+        return_value = int(value)
+    is_float = re.search(r"^[0-9]+\.[0-9]*$", value)
+    if is_float:
+        return_value = float(value)
+    is_bool = re.search(r"^true$|^false$", value)
+    if is_bool:
+        return_value = str_to_bool(value)
+    return return_value
